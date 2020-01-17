958c4a4192dd3aeb0e000122e5feba5cf4320a9c
Ryan Georgioff <rpg89@keemail.me>
2020-01-16 22:46:15 -0800

    add au fond rose

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # tf-nst
 
 This is a working repo for experiments in neural style transfer, a relatively new technique which uses extracted features from machine vision algorithm "vgg19" to map features from one image to another.
+
+First attempt:
+![Portrait de Victor Chocquet, par Paul Cézanne](https://github.com/atrusofthefuture/tf-nst/blob/master/best_cezanne.jpg)
+
+Second attempt:
+![Portrait de l'artiste au fond rose, par Paul Cézanne #1](https://github.com/atrusofthefuture/tf-nst/blob/master/best-lartiste.png)
+![Comparison of au fond rose](https://github.com/atrusofthefuture/tf-nst/blob/master/lartiste-output.png)
+
+Third attempt:
+![Portrait de l'artiste, par Paul Cézanne #2](https://github.com/atrusofthefuture/tf-nst/blob/master/best-lartiste2.png)
--- nst.py
+++ nst.py
@@ -2,12 +2,6 @@ import os
 img_dir = '/tmp/nst'
 if not os.path.exists(img_dir):
     os.makedirs(img_dir)
-!wget --quiet -P /tmp/nst/ https://upload.wikimedia.org/wikipedia/commons/d/d7/Green_Sea_Turtle_grazing_seagrass.jpg
-!wget --quiet -P /tmp/nst/ https://upload.wikimedia.org/wikipedia/commons/0/0a/The_Great_Wave_off_Kanagawa.jpg
-!wget --quiet -P /tmp/nst/ https://upload.wikimedia.org/wikipedia/commons/b/b4/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg
-!wget --quiet -P /tmp/nst/ https://upload.wikimedia.org/wikipedia/commons/0/00/Tuebingen_Neckarfront.jpg
-!wget --quiet -P /tmp/nst/ https://upload.wikimedia.org/wikipedia/commons/6/68/Pillars_of_creation_2014_HST_WFC3-UVIS_full-res_denoised.jpg
-!wget --quiet -P /tmp/nst/ https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1024px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
